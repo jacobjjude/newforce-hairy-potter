5270060e8d1794120d573ab2236be5264b60439b
Jacob Jude <jacobj.jude@outlook.com>
2022-06-21 11:33:01 -0400

    added pricing and cracked/not cracked for pottery

---
+++ src/scripts/PotteryCatalog.js
@@ -0,0 +1,20 @@
+let sellPottery = [];
+
+
+export const toSellOrNottoSell = (obj) => {
+	if (obj.weight <= 6) {
+		obj.price = 40;
+	} else if (obj.weight > 6) {
+		obj.price = 20;
+	}
+
+	if (obj.cracked === false) {
+		sellPottery.push(obj);
+	}
+
+	return obj;
+};
+
+export const usePottery = () => {
+    return sellPottery.map(pot => ({...pot}))
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,37 @@
 // Imports go first
-import { makePottery } from "./PotteryWheel.js";
-import { firePottery } from "./Kiln.js";
+import { makePottery } from './PotteryWheel.js';
+import { firePottery } from './Kiln.js';
+import { toSellOrNottoSell } from './PotteryCatalog.js';
 
 function randNum() {
-    let num = Math.floor((Math.random())*10 +1);
-    return num
+	let num = Math.floor(Math.random() * 10 + 1);
+	return num;
 }
 
 function randTemp() {
-    let temp = Math.floor((Math.random() + 2)*1000);
-    return temp
+	let temp = Math.floor((Math.random() + 1.7) * 1000);
+	return temp;
 }
 
-let shapes = ['mug', 'plate', 'vase', 'pipe', 'coffee cup']
+let shapes = [ 'mug', 'plate', 'vase', 'pipe', 'coffee cup' ];
 let pottery = [];
+
 // Make 5 pieces of pottery at the wheel
 for (const shape of shapes) {
-    let clay = makePottery(shape, randNum(), randNum());
-    pottery.push(clay)
+	let clay = makePottery(shape, randNum(), randNum());
+	pottery.push(clay);
 }
 
 // Fire each piece of pottery in the kiln
 for (const pot of pottery) {
-    let inTheKiln = firePottery(pot, randTemp())
+	let inTheKiln = firePottery(pot, randTemp());
 }
 
-console.log(pottery)
-
 // Determine which ones should be sold, and their price
+for (const pot of pottery) {
+	let cracked = toSellOrNottoSell(pot);
+}
 
+console.log(pottery);
 
 // Invoke the component function that renders the HTML list
-
-
-
